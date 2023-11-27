397d2f0a9d05b1103399da22100d6c05f1d931c0
Toxe <toxe@toxe.net>
2023-11-27 09:21:37 +0100

    Add alternative method using trigonometric functions

--- WeaponSway/weapon.gd
+++ WeaponSway/weapon.gd
@@ -6,6 +6,20 @@ extends Node2D
 @onready var initial_position := position
 
 
+func calc_with_curves(time: float) -> Vector2:
+    var t := time - int(time)
+    return Vector2(horizontal_sway.sample(t), vertical_sway.sample(t))
+
+
+func calc_with_trig(time: float) -> Vector2:
+    var f := time * 2.0 * PI
+    return Vector2(sin(f), 1.0 - absf(cos(f)))
+
+
 func _process(_delta: float) -> void:
-    var t := wrapf(Time.get_unix_time_from_system(), 0.0, 1.0)
-    position = initial_position + Vector2(horizontal_sway.sample(t) * 50.0, -vertical_sway.sample(t) * 50.0)
+    var time := Time.get_unix_time_from_system()
+
+    var offset := calc_with_curves(time)
+    # var offset := calc_with_trig(time)
+
+    position = initial_position + offset * Vector2(50, -50)
