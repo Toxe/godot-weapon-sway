9145ec47814ac7d6863a61e88e229ff9be87a4b6
Toxe <toxe@toxe.net>
2025-04-28 13:40:08 +0200

    Background animation tween did not properly repeat

--- WeaponSway/main.gd
+++ WeaponSway/main.gd
@@ -3,5 +3,5 @@ extends Node2D
 
 func _ready() -> void:
     var tween := create_tween()
-    tween.tween_property($Background/Tiles, "position", Vector2(0, 800), 5)
+    tween.tween_property($Background/Tiles, "position", Vector2(0, 800), 5).from(Vector2(0, 0))
     tween.set_loops()
